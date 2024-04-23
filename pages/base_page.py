52af3e7350a1b54a5baa62085088904f5f551600
Olga Minina <arb.test2020@gmail.com>
2024-04-23 10:22:50 +0200

    add exception to base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
-class BasePage():
-    def __init__(self, browser, url):
+from selenium.common.exceptions import NoSuchElementException
+
+
+class BasePage:
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        # self.browser.implicity_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):  # 2 аргумента - как искать
+        # (css, id, xpath и тд) и собственно что искать (строку-селектор).
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
