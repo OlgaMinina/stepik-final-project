ffa0f84501d5d55301ecbd250775708aa5f6779b
Olga Minina <arb.test2020@gmail.com>
2024-04-24 10:06:03 +0200

    change ProductPageLocators names

--- pages/locators.py
+++ pages/locators.py
@@ -9,7 +9,7 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, "button[value='Add to basket']")
-    ALERT_MESSAGE = (By.XPATH, "//div[@id='messages']//div[1]//div[1]")
-    PRICE_MESSAGE = (By.CSS_SELECTOR, "div[class = 'alertinner '] p strong")
+    ADDED_PRODUCT_NAME = (By.XPATH, "//div[@id='messages']//div[1]//div[1]//strong")
+    ADDED_PRODUCT_PRICE = (By.CSS_SELECTOR, "div[class = 'alertinner '] p strong")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
