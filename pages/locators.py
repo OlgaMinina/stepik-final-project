83b67b4c7dc8a46594e9d4ff56bb10135935061e
Olga Minina <arb.test2020@gmail.com>
2024-04-24 08:53:09 +0200

    edit for adding product page

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "button[value='Add to basket']")
+    ALERT_MESSAGE = (By.XPATH, "//div[@id='messages']//div[1]//div[1]")
+    PRICE_MESSAGE = (By.CSS_SELECTOR, "div[class = 'alertinner '] p strong")
